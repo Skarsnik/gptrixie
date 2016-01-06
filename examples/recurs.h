8bef3c6ba964b57476d68a0898d0ce982b69d6bf
Ben Petering <ben@petering.net>
2016-01-07 09:03:52 +1030

    Add some missing NativeCall types; Put commented function definition above generated sub; Use Buf instead of Pointer[uint8] where appropriate

---
+++ examples/recurs.h
@@ -0,0 +1,11 @@
+#ifndef RECURS_H
+#define RECURS_H
+
+/* pull in other header files. try to match typical structure of
+   a proper production lib */
+
+#include "recurs/common.h"
+#include "recurs/test.h"
+#include "recurs/test2.h"
+
+#endif
---
+++ examples/recurs/test2.h
@@ -0,0 +1,9 @@
+#ifndef TEST2_H
+#define TEST2_H
+
+#define RANDOM_CONSTANT 42
+
+/* gccxml doesn't pick up this file with just RANDOM_CONSTANT - why not? */
+int x();
+
+#endif
